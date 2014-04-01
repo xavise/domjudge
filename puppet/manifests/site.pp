2d092aaf151b1ff62a25d51266a44240a53c290d
Alex Muntada <alexm@alexm.org>
2014-04-01 18:14:30 +0200

    install domjudge-domserver

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -1,1 +1,15 @@
 class { 'apt': always_apt_update => true }
+
+apt::source { 'domjudge':
+  location          => 'http://non-gnu.uvt.nl/debian',
+  release           => 'squeeze',
+  repos             => 'uvt',
+  key               => '211A1230',
+  key_server        => 'pgp.mit.edu',
+}
+
+package {
+  'domjudge-domserver':
+    ensure  => present,
+    require => Apt::Source['domjudge']
+}
